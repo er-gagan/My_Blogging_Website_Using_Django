ac7eea5523917036bf0a69f097511845a4e43e63
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-03-23 16:48:43 +0530

    Creating Models
    Create Database ORM

--- MyBlog/MyHomeApp/models.py
+++ MyBlog/MyHomeApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    S_No = models.AutoField(primary_key=True)
+    Name=models.CharField(max_length=100)
+    Email=models.EmailField(max_length=100)
+    Phone=models.CharField(max_length=15)
+    Content=models.CharField(max_length=300)
+    timeStamp = models.DateTimeField(auto_now_add=True,blank=True)
