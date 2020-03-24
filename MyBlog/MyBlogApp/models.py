e948f87d95ac38bf78e6b8fc59f0f0b993f3f8fc
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-03-24 19:22:42 +0530

    Adding Blog
    Blog Add into Database and render into html file

--- MyBlog/MyBlogApp/models.py
+++ MyBlog/MyBlogApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    S_No = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=200)
+    Content = models.TextField()
+    Author = models.CharField(max_length=100)
+    slug = models.CharField(max_length=200)
+    TimeStamp = models.DateTimeField(blank=True)
+    
+    def __str__(self):
+        return self.Title
