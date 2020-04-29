d8c91932f37a1e49f61ad18f79b5a00b06c7546a
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-04-29 22:43:02 +0530

    Creating Virtual env
    Virtual env create

---
+++ MyBlogEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gagan\OneDrive\Documents\Github\My_Blogging_Website_Using_Django\MyBlogEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
