ab38f5e7c4a13b97378f1f74201895c115fdeaa1
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-04-29 23:45:57 +0530

    Adding humanize feature
    naturaline as humanize load on comment section timestamp

--- MyBlogEnv/MyBlog/MyBlog/settings.py
+++ MyBlogEnv/MyBlog/MyBlog/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'MyBlogApp',
     'MyHomeApp',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
