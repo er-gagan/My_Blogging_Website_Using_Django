968ead68b7f5b3dbd75362a6708bb6bf968ddf57
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-05-03 00:53:28 +0530

    Adding custom tamplates and reply
    Reply functionality add and create custom templates

--- MyBlogEnv/MyBlog/MyBlogApp/views.py
+++ MyBlogEnv/MyBlog/MyBlogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from MyBlogApp.models import Post,BlogComment
+from MyBlogApp.templatetags import extras
 # Create your views here.
 def blogHome(request):
     allPosts = Post.objects.all()
@@ -9,8 +10,15 @@ def blogHome(request):
 
 def blogPost(request,slug):
     post = Post.objects.filter(slug=slug).first()
-    comments = BlogComment.objects.filter(BlogPost=post)
-    context = {'post':post,'comments':comments,'user':request.user}
+    comments = BlogComment.objects.filter(BlogPost=post,Parent=None)
+    replies = BlogComment.objects.filter(BlogPost=post).exclude(Parent=None)
+    replyDict = {}
+    for reply in replies:
+        if reply.Parent.S_No not in replyDict.keys():
+            replyDict[reply.Parent.S_No] = [reply]
+        else:
+            replyDict[reply.Parent.S_No].append(reply)
+    context = {'post':post,'comments':comments,'user':request.user,'replyDict':replyDict}
     return render(request,"blog/blogPost.html",context)
 
 def postComment(request):
@@ -19,6 +27,16 @@ def postComment(request):
         User = request.user
         Post_S_No = request.POST["postSno"]
         BlogPost = Post.objects.get(S_No=Post_S_No)
-        BlogComment(comment=comment, User=User, BlogPost=BlogPost).save()
-        messages.success(request,"Your comment has been posted successfully")
+        
+        parentSno = request.POST.get("parentSno")
+        if parentSno == "":
+            comment = BlogComment(comment=comment, User=User, BlogPost=BlogPost)
+            comment.save()
+            messages.success(request,"Your comment has been posted successfully")
+        else:
+            Parent = BlogComment.objects.get(S_No=parentSno)
+            comment = BlogComment(comment=comment, User=User, BlogPost=BlogPost, Parent=Parent)
+            comment.save()
+            messages.success(request,"Your reply has been posted successfully")
+            
     return redirect(f"/blog/{BlogPost.slug}")
